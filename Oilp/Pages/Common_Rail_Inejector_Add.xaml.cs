efe2010c8e3eed905aec33786fe751520fc32119
Slytherin <403920986@qq.com>
2018-06-06 17:35:55 +0800

    设置界面

--- Oilp/Pages/Common_Rail_Inejector_Add.xaml.cs
+++ Oilp/Pages/Common_Rail_Inejector_Add.xaml.cs
@@ -48,6 +48,10 @@ namespace OilP.Pages
          * */
         private void step_ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (step_ListBox.Items.Count<1)
+            {
+                return;
+            }
             string step_name = ((ListBoxItem)step_ListBox.SelectedItem).Content.ToString();
             TEMPLATE_Model tEMPLATE_Model = new TEMPLATE_Model();
             foreach (var item in DATA)
@@ -59,6 +63,35 @@ namespace OilP.Pages
                 }
             }
             /*在页面上填入数据*/
+            FillData(tEMPLATE_Model);
+        }
+
+        /**
+         * 页面填入数据
+         * */
+         public void FillData(TEMPLATE_Model tEMPLATE_Model)
+        {
+
+            last_time_TextBox.Text = tEMPLATE_Model.Pulse_width;
+            pressure_TextBox.Text = tEMPLATE_Model.Rail_pressure.ToString();
+            oil_p_hor_TextBox.Text = tEMPLATE_Model.Oil_p_standard;
+            oil_p_ver_TextBox.Text = tEMPLATE_Model.Oil_p_deviation;
+            oil_h_hor_TextBox.Text = tEMPLATE_Model.Oil_h_standard;
+            oil_h_ver_TextBox.Text = tEMPLATE_Model.Oil_h_pressure;
+            //脉宽和时间的区分？
+            test_time_TextBox.Text = tEMPLATE_Model.Control_last_time;
+            if (null == tEMPLATE_Model.Curve || tEMPLATE_Model.Curve.Length == 0)
+            {
+                curve_TextBox.Text = null;
+            }
+            else
+            {
+                curve_TextBox.Text = tEMPLATE_Model.Curve.ToString();
+            }
+            magn_TextBox.Text = tEMPLATE_Model.Manufacturer;
+            period_TextBox.Text = "未设定";
+            voltage_TextBox.Text = tEMPLATE_Model.Voltage;
+            //读取数据后设置不可读
 
         }
 
@@ -69,14 +102,19 @@ namespace OilP.Pages
         public void Init_step_ListBox(List<TEMPLATE_Model> tEMPLATE_Models)
         {
             step_ListBox.Items.Clear();
+            ListBoxItem listBoxItem = new ListBoxItem();
+            int i = 1;
             foreach (TEMPLATE_Model item in tEMPLATE_Models)
             {
                 ListBoxItem temp = new ListBoxItem();
                 temp.Content = item.Step_name;
                 temp.FontSize = 40;
-                temp.FontFamily = new FontFamily("Yu Gothic UI Semibold");
+                temp.FontFamily = new FontFamily("Yu Gothic UI Semibold");              
                 step_ListBox.Items.Add(temp);
+                i++;
             }
+            //默认选中第一项加载数据
+            step_ListBox.SelectedIndex = 0;
         }
 
         /**
@@ -84,9 +122,16 @@ namespace OilP.Pages
          * */
         private void template_combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string temp_no = template_combobox.Text;
+            
+            string temp_no = template_combobox.SelectedValue.ToString();
+            temp_no = temp_no.Replace("System.Windows.Controls.ComboBoxItem: ", "");
             if (temp_no.Length<1||"请选择".Equals(temp_no))
             {
+                if (step_ListBox!=null)
+                {
+                    step_ListBox.Items.Clear();
+                }
+               
                 return;
             }
             List<TEMPLATE_Model> tEMPLATE_Models = new List<TEMPLATE_Model>();
@@ -94,6 +139,14 @@ namespace OilP.Pages
             DATA = tEMPLATE_Models;
             /*初始化listBox*/
             Init_step_ListBox(tEMPLATE_Models);
+        }
+
+        /**
+         * 保存新建的数据，创建一个新的txt文件
+         * */
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
