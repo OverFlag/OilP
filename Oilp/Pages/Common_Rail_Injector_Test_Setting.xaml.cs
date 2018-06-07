360a4f4b6f16f195a148ae03c37bfa670affade0
Slytherin <403920986@qq.com>
2018-06-07 19:37:57 +0800

    修改设置界面

---
+++ Oilp/Pages/Common_Rail_Injector_Test_Setting.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OilP.Pages
+{
+    /// <summary>
+    /// Common_Rail_Injector_Test_Setting.xaml 的交互逻辑
+    /// </summary>
+    public partial class Common_Rail_Injector_Test_Setting : Page
+    {
+        public Common_Rail_Injector_Test_Setting()
+        {
+            InitializeComponent();
+        }
+    }
+}
